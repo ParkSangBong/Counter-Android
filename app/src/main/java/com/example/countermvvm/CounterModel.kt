884f9d02ee8bf8c9e931b3d6746eced60944d5a6
ParkSangBong <meca231@gmail.com>
2025-03-13 23:59:36 +0900

    modify CounterModel

--- app/src/main/java/com/example/countermvvm/CounterModel.kt
+++ app/src/main/java/com/example/countermvvm/CounterModel.kt
@@ -1,4 +1,17 @@
 package com.example.countermvvm
 
-class CounterModel {
+data class CounterModel (var count: Int)
+
+class CounterRepository {
+    private var _counter = CounterModel(0)
+
+    fun getCounter() = _counter
+
+    fun incrementCounter() {
+        _counter.count++
+    }
+
+    fun decrementCounter() {
+        _counter.count--
+    }
 }
